e32068e5809f9354d06e7d8511e3e5158f98832c
Guilherme Santiago da Cruz <guilhermesantiagodacruz@gmail.com>
2026-04-25 23:14:19 -0300

    Add files via upload

---
+++ Exemplo7_NumerosInteiros.cs
@@ -0,0 +1,20 @@
+using System;
+
+public Class
+{
+    static void Main(string[] args)
+    {
+        int a, b, c;
+        Console.Write("Digite um numero: ");
+        a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Digite outro numero: ");
+        b = Convert.ToInt32(Console.ReadLine());
+
+        c = a + b;
+
+        Console.Write(a + " + " + b + "=" + c);
+
+        Console.ReadKey();
+    }
+}
