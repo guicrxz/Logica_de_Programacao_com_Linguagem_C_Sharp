80cf70cbb6f3fff9a93937bd3853c278af114155
Guilherme Santiago da Cruz <guilhermesantiagodacruz@gmail.com>
2026-04-26 17:46:49 -0300

    Add files via upload

---
+++ Exemplo3_EstruturaCondComposta1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int N, R;
+        Console.Write("Digite um número: ");
+        N = int.Parse(Console.ReadLine());
+        
+        R = N % 2;      //cálculo do resto da divisão de N por 2
+        
+        if (R == 0)
+            Console.WriteLine("\n{0} é par", N);
+        else
+            Console.WriteLine("\n{0} é ímpar", N);
+            
+        Console.ReadKey();
+    }
+}
---
+++ Exemplo3_EstruturaCondComposta2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int N;
+        Console.Write("Digite um número: ");
+        N = int.Parse(Console.ReadLine());
+        
+        if ((N % 2) == 0)
+            Console.WriteLine("\n{0} é par", N);
+        else
+            Console.WriteLine("\n{0} é ímpar", N);
+            
+        Console.ReadKey();
+    }
+}
