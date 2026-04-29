ad5637f8b0969050bbdc00cf0d3b9cf187fdea8d
Guilherme Santiago da Cruz <guilhermesantiagodacruz@gmail.com>
2026-04-29 13:20:55 -0300

    Add files via upload

---
+++ Exemplo10.cs
@@ -0,0 +1,15 @@
+using System;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int x;
+        
+        for (x = 10; x >= 1; x--)
+        {
+            Console.WriteLine("{0}", x);
+        }
+        Console.ReadKey();
+    }
+}
